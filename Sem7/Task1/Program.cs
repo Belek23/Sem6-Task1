774ac7eca56ceb1bb41887901d42a84f77e8c6b4
Belek Abdy <beshik2013@yandex.ru>
2024-07-17 14:55:07 +0300

    add Seminar7:Task1

---
+++ Sem7/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр.
+// Указание: использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+int SumOfDigits(int num)
+{
+    if (num > 0)
+    {
+        return num % 10 + SumOfDigits(num / 10);
+    }
+    return 0;
+}
+System.Console.WriteLine(SumOfDigits(123));
